8932ca01939d2da844c230ff802c2e3a7951da53
camilleg <camilleg@camille-g.com>
2021-12-29 21:59:47 -0600

    unused

--- tb303/tb303.h
+++ tb303/tb303.h
@@ -9,7 +9,7 @@ class tb303Alg : public VAlgorithm
 	float vcf_cutoff, vcf_envmod, vcf_envdecay, vcf_reso, vcf_rescoeff;
 	float vcf_e0, vcf_e1;
 	float vcf_c0; // c0=e1 on retrigger, c0*=ed every sample
-	float vcf_d1, vcf_d2, vcf_envpos;
+	float vcf_d1, vcf_d2;
 	int fStarted;
 	void recalc();
 
