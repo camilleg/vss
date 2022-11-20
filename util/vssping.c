db42ecf2eca06ab304869dd752c4d47cf8fce104
camilleg <camilleg@camille-g.com>
2022-11-19 22:12:48 -0600

    exit status

--- util/vssping.c
+++ util/vssping.c
@@ -1,13 +1,9 @@
-/**************************************************
- *                    vssPing.c                   *
- * The simplest possible Audition client.  Open a *
- * a connection to the server and then close it.  *
- **************************************************/
 #include "vssClient.h"
 
 int main()
 {
-	if (BeginSoundServer())
-		EndSoundServer();
-	return 0;
+  if (!BeginSoundServer())
+    return 1;
+  EndSoundServer();
+  return 0;
 }
